914cf1db1937a6c874cba0c5106d230a5aeeaef0
Dukhnich Vlad <vladd@smartinfosys.com>
2018-03-14 11:39:20 +0300

    mogo to mLab

--- accounting/config.js
+++ accounting/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'issuer': "qergjqei;ruhgqirng2q;45yh125h4tb5q21",
     'secret': 's@iHall0B1ache3',
-    'database': 'mongodb://test2:initial-password@localhost:27017/',
+    'database': 'mongodb://test2:initial-password@ds012198.mlab.com:12198/accounting',
     'appRoot': __dirname,
     'defaultPort': 8081
 };
--- identity/config.js
+++ identity/config.js
@@ -3,7 +3,7 @@ const qs = require('qs');
 module.exports = {
     'issuer': "qergjqei;ruhgqirng2q;45yh125h4tb5q21",
     'secret': 's@iHall0B1ache3',
-    'database': 'mongodb://test2:initial-password@localhost:27017/',
+    'database': 'mongodb://test2:initial-password@ds012178.mlab.com:12178/identity',
     'appRoot': __dirname,
     'defaultPort': '8080',
     'accounting': 'localhost',
